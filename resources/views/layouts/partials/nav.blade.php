fa8ca87e58c82795c8e6920c597fab2ca8d9d9a1
Harmhtn <Harmhtn@gmail.com>
2020-07-27 21:33:19 +0200

    style changes to homepage

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -1,4 +1,8 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<head>
+    <link href="{{ asset('css/main.style.css') }}" rel="stylesheet">
+</head>
+
+<div class="navbar navbar-expand-lg navbar-light bg-light ">
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -19,7 +23,7 @@
             </li>
         </ul>
     </div>
-</nav>
+</div>
 
 
 
